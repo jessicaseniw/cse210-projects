1e5ffb6b682d5c846fea9a04ad21cf8e9ba86f7d
jessicaseniw <jessica.seniw@hotmail.com>
2024-03-29 00:16:32 -0300

    learning activity w05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,88 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Shape
 {
-    static void Main(string[] args)
+    private string color;
+
+    public Shape(string color)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        this.color = color;
+    }
+
+    public string GetColor()
+    {
+        return color;
+    }
+
+    public virtual float GetArea()
+    {
+        return 0f; // Base implementation, to be overridden by derived classes
+    }
+}
+
+public class Square : Shape
+{
+    private float side;
+
+    public Square(string color, float side) : base(color)
+    {
+        this.side = side;
+    }
+
+    public override float GetArea()
+    {
+        return side * side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private float length;
+    private float width;
+
+    public Rectangle(string color, float length, float width) : base(color)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override float GetArea()
+    {
+        return length * width;
+    }
+}
+
+public class Circle : Shape
+{
+    private float radius;
+
+    public Circle(string color, float radius) : base(color)
+    {
+        this.radius = radius;
+    }
+
+    public override float GetArea()
+    {
+        return (float)Math.PI * radius * radius;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        // Create instances of different shapes
+        shapes.Add(new Square("Red", 4f));
+        shapes.Add(new Rectangle("Blue", 3f, 5f));
+        shapes.Add(new Circle("Green", 2.5f));
+
+        // Display color and area of each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
