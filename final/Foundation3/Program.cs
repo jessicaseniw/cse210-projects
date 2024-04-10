ecc7905101091891e0305ee7bbd31bf2310e2a05
jessicaseniw <jessica.seniw@hotmail.com>
2024-04-09 22:44:41 -0300

    adding Foundation 3 and 4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,143 @@
 using System;
 
-class Program
+// Class representing an address
+public class Address
 {
-    static void Main(string[] args)
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string StateProvince { get; private set; }
+    public string Country { get; private set; }
+
+    public Address(string street, string city, string stateProvince, string country)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Street = street;
+        City = city;
+        StateProvince = stateProvince;
+        Country = country;
+    }
+
+    public override string ToString()
+    {
+        return $"{Street}, {City}, {StateProvince}, {Country}";
+    }
+}
+
+// Base class representing an event
+public abstract class Event
+{
+    private string _eventTitle;
+    private string _description;
+    private DateTime _date;
+    private string _time;
+    private Address _eventAddress;
+
+    public Event(string title, string description, DateTime date, string time, Address address)
+    {
+        _eventTitle = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _eventAddress = address;
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"Event: {_eventTitle}\nDescription: {_description}\nDate: {_date.ToShortDateString()}\nTime: {_time}\nAddress: {_eventAddress}\n";
+    }
+
+    public abstract string GetFullDetails();
+
+    public string GetShortDescription()
+    {
+        return $"Type: {GetType().Name}\nEvent: {_eventTitle}\nDate: {_date.ToShortDateString()}\n";
+    }
+}
+
+// Derived class for lectures
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacity;
+
+    public Lecture(string title, string description, DateTime date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"Type: Lecture\nSpeaker: {_speaker}\nCapacity: {_capacity}\n";
+    }
+}
+
+// Derived class for receptions
+public class Reception : Event
+{
+    private string _rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"Type: Reception\nRSVP Email: {_rsvpEmail}\n";
+    }
+}
+
+// Derived class for outdoor gatherings
+public class OutdoorGathering : Event
+{
+    private string _weatherForecast;
+
+    public OutdoorGathering(string title, string description, DateTime date, string time, Address address, string weatherForecast)
+        : base(title, description, date, time, address)
+    {
+        _weatherForecast = weatherForecast;
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"Type: Outdoor Gathering\nWeather Forecast: {_weatherForecast}\n";
+    }
+}
+
+// Main program
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create addresses
+        Address address1 = new Address("123 Main St", "Anytown", "CA", "USA");
+        Address address2 = new Address("456 Oak Ave", "Othercity", "NY", "USA");
+        Address address3 = new Address("789 Elm St", "Anothercity", "TX", "USA");
+
+        // Create events
+        Event lectureEvent = new Lecture("Introduction to Programming", "Learn the basics of programming", DateTime.Parse("2024-04-15"), "10:00 AM", address1, "John Smith", 50);
+        Event receptionEvent = new Reception("Networking Reception", "Join us for networking and refreshments", DateTime.Parse("2024-05-20"), "6:00 PM", address2, "info@example.com");
+        Event outdoorEvent = new OutdoorGathering("Summer Picnic", "Enjoy a picnic in the park", DateTime.Parse("2024-06-30"), "12:00 PM", address3, "Sunny with a chance of showers");
+
+        // Display marketing messages for each event
+        Console.WriteLine("Marketing Messages:");
+        Console.WriteLine("===================");
+
+        Console.WriteLine("\nStandard Details:");
+        Console.WriteLine(lectureEvent.GetStandardDetails());
+        Console.WriteLine(receptionEvent.GetStandardDetails());
+        Console.WriteLine(outdoorEvent.GetStandardDetails());
+
+        Console.WriteLine("\nFull Details:");
+        Console.WriteLine(lectureEvent.GetFullDetails());
+        Console.WriteLine(receptionEvent.GetFullDetails());
+        Console.WriteLine(outdoorEvent.GetFullDetails());
+
+        Console.WriteLine("\nShort Descriptions:");
+        Console.WriteLine(lectureEvent.GetShortDescription());
+        Console.WriteLine(receptionEvent.GetShortDescription());
+        Console.WriteLine(outdoorEvent.GetShortDescription());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,132 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+// Base class representing an activity
+public abstract class Activity
 {
-    static void Main(string[] args)
+    protected DateTime ActivityDate { get; }
+    protected int ActivityLengthMinutes { get; }
+
+    protected Activity(DateTime date, int lengthMinutes)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        ActivityDate = date;
+        ActivityLengthMinutes = lengthMinutes;
+    }
+
+    public abstract double CalculateDistance();
+    public abstract double CalculateSpeed();
+    public abstract double CalculatePace();
+
+    public virtual string GetSummary()
+    {
+        double distance = CalculateDistance();
+        double speed = CalculateSpeed();
+        double pace = CalculatePace();
+
+        return $"{ActivityDate.ToShortDateString()} {GetType().Name} ({ActivityLengthMinutes} min) - Distance: {distance:F2} miles, Speed: {speed:F2} mph, Pace: {pace:F2} min per mile";
+    }
+}
+
+// Derived class for running activity
+public class Running : Activity
+{
+    public double DistanceInMiles { get; }
+
+    public Running(DateTime date, int lengthMinutes, double distanceMiles)
+        : base(date, lengthMinutes)
+    {
+        DistanceInMiles = distanceMiles;
+    }
+
+    public override double CalculateDistance()
+    {
+        return DistanceInMiles;
+    }
+
+    public override double CalculateSpeed()
+    {
+        return (DistanceInMiles / ActivityLengthMinutes) * 60;
+    }
+
+    public override double CalculatePace()
+    {
+        return ActivityLengthMinutes / DistanceInMiles;
+    }
+}
+
+// Derived class for cycling activity
+public class Cycling : Activity
+{
+    public double SpeedInKph { get; }
+
+    public Cycling(DateTime date, int lengthMinutes, double speedKph)
+        : base(date, lengthMinutes)
+    {
+        SpeedInKph = speedKph;
+    }
+
+    public override double CalculateSpeed()
+    {
+        return SpeedInKph;
+    }
+
+    public override double CalculateDistance()
+    {
+        return (SpeedInKph * ActivityLengthMinutes) / 60;
+    }
+
+    public override double CalculatePace()
+    {
+        return 60 / SpeedInKph;
+    }
+}
+
+// Derived class for swimming activity
+public class Swimming : Activity
+{
+    public int Laps { get; }
+
+    public Swimming(DateTime date, int lengthMinutes, int laps)
+        : base(date, lengthMinutes)
+    {
+        Laps = laps;
+    }
+
+    public override double CalculateDistance()
+    {
+        return Laps * 50 / 1000 * 0.62; // Convert laps to miles
+    }
+
+    public override double CalculateSpeed()
+    {
+        return (CalculateDistance() / ActivityLengthMinutes) * 60;
+    }
+
+    public override double CalculatePace()
+    {
+        return ActivityLengthMinutes / CalculateDistance();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create list of different activity types
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(DateTime.Parse("2024-04-03"), 30, 3.0),
+            new Cycling(DateTime.Parse("2024-04-03"), 30, 12.0),
+            new Swimming(DateTime.Parse("2024-04-03"), 30, 20)
+        };
+
+        // Display summary information for each activity
+        Console.WriteLine("Activity Summaries:");
+        Console.WriteLine("===================");
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
